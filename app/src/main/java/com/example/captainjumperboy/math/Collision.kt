70d8081a1a67ad82a332441a2683cd57d5d0b7ce
Matthias <tom4tto@outlook.com>
2023-02-03 03:22:29 +0800

    implement AABB collision + resolution
    yet to be tested

---
+++ app/src/main/java/com/example/captainjumperboy/math/Collision.kt
@@ -0,0 +1,48 @@
+package com.example.captainjumperboy.math
+
+import kotlin.math.abs
+
+class Collision {
+    class AABB(var pos: Vector2D, var halfSize: Vector2D) {
+
+        //for collision detection
+        fun collidesWith(other: AABB): Boolean {
+            val distance = pos.add(other.pos.add(other.halfSize.add(halfSize.negate())))
+            //if distance with other AABB is < halfSize = COLLIDE
+            return abs(distance.x) <= (halfSize.x + other.halfSize.x) &&
+                    abs(distance.y) <= (halfSize.y + other.halfSize.y)
+        }
+
+        //for collision resolution between 2 AABBs
+        fun resolveOverlap(other: AABB) {
+            // get vector between the centers of the two rectangles
+            val distance = pos.add(other.pos.add(other.halfSize.add(halfSize.negate())))
+
+            // GET amount of overlap in the x and y axis
+            val overlap = Vector2D(
+                (halfSize.x + other.halfSize.x) - abs(distance.x),
+                (halfSize.y + other.halfSize.y) - abs(distance.y)
+            )
+
+            //RESOLUTION IDEA: offset the position by overlap amount
+            // NOTE: to prevent objects from tunneling through each other so resolve smaller overlap
+
+            // If smallest overlap is in the x axis, resolve the overlap in that direction
+            if (overlap.x < overlap.y) {
+                if (distance.x > 0) {
+                    pos = pos.add(Vector2D(overlap.x, 0f))
+                } else {
+                    pos = pos.add(Vector2D(-overlap.x, 0f)) //add negative offset to positive overlap
+                }
+            }
+            // if the smallest overlap is in the y axis, resolve overlap y
+            else {
+                if (distance.y > 0) {
+                    pos = pos.add(Vector2D(0f, overlap.y))
+                } else {
+                    pos = pos.add(Vector2D(0f, -overlap.y)) //add negative offset to positive overlap
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/captainjumperboy/math/Vector2D.kt
+++ app/src/main/java/com/example/captainjumperboy/math/Vector2D.kt
@@ -1,4 +1,12 @@
 package com.example.captainjumperboy.math
 
 data class Vector2D(var x : Float = 0F, var y: Float = 0F){
+    fun add(v: Vector2D): Vector2D {
+        return Vector2D(x + v.x, y + v.y)
+    }
+    fun negate(): Vector2D {
+        x *= -1
+        y *= -1
+        return this
+    }
 }
