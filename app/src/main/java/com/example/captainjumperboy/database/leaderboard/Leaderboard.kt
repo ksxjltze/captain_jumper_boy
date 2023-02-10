669288aec41e9211c43e916b211941be14684d69
Matthias <tom4tto@outlook.com>
2023-02-10 17:13:41 +0800

    create leaderboard and dao

---
+++ app/src/main/java/com/example/captainjumperboy/database/leaderboard/Leaderboard.kt
@@ -0,0 +1,20 @@
+package com.example.captainjumperboy.database.leaderboard
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+//@Entity for Room to recognize this class as something that can be used to define database tables
+//association between the class and the database using Kotlin annotations
+
+/**
+ * This represents that the Leaderboard class can be represented as a database table
+ */
+@Entity(tableName = "leaderboard_table")
+data class Leaderboard(
+    //Properties
+    //Room will ultimately use these properties to both create the table and instantiate objects from rows in the database.
+    @PrimaryKey(autoGenerate = true) var id: Int = 0,
+    @ColumnInfo(name = "name") val name: String,
+    @ColumnInfo(name = "score") val score: Int
+)
