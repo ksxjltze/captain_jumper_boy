54ca2b2e3064800627bbacd244e7ecd04f7bb684
Darrick Ching <darrick.c@digipen.edu>
2023-02-23 13:31:48 +0800

    update credits

--- app/src/main/java/com/stepbros/captainjumperboy/ui/CreditsActivity.kt
+++ app/src/main/java/com/stepbros/captainjumperboy/ui/CreditsActivity.kt
@@ -1,6 +1,9 @@
 package com.stepbros.captainjumperboy.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.stepbros.captainjumperboy.R
 
@@ -12,7 +15,14 @@ class CreditsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_credits)
 
-
+        val backBtn: Button = findViewById(R.id.backBtn)
+        
+        backBtn.setOnClickListener {
+            Toast.makeText(this, "Back to Main Menu!", Toast.LENGTH_SHORT).show()
+            //Go to leaderboard
+            val intent = Intent(this, MainMenuActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 }
