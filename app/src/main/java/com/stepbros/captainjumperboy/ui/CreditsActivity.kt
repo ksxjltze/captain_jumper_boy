279be8eb6254ccfc378118847159edec2faa7a37
Darrick Ching <darrick.c@digipen.edu>
2023-02-23 13:28:22 +0800

    added credits

---
+++ app/src/main/java/com/stepbros/captainjumperboy/ui/CreditsActivity.kt
@@ -0,0 +1,18 @@
+package com.stepbros.captainjumperboy.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.stepbros.captainjumperboy.R
+
+
+
+class CreditsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_credits)
+
+
+    }
+
+}
